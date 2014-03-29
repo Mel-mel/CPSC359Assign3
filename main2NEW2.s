8fcad3ae61818c13ae80b96b7154c94bdbf7a73f
emCsq <emcsq@live.ca>
2014-03-28 21:47:42 -0600

    Create main2NEW2.s

---
+++ main2NEW2.s
@@ -0,0 +1,45 @@
+.section    .init
+.globl     _starting
+
+_starting:
+    b       main
+    
+.section .text
+bleh:
+main:
+    mov     sp, #0x8000
+
+	bl		EnableJTAG
+	bl		InitFrameBuffer
+	mov		r12,	#900
+	mov		r7,	#0
+	mov		r8,	#150
+
+.globl loopMain
+loopMain:
+	bl drawMap
+testing:
+	cmp	r12,	#1016
+	beq	attempt
+looping:
+	bl controllerInit
+	mov		r9,	r0
+	bl		InitFrameBuffer
+	bl redraw
+bam:
+	b	loopMain
+attempt:
+	bl 		controllerInit
+	mov		r9,	r0
+	bl		InitFrameBuffer
+	bl		rerun
+	cmp		r12,	#900
+	beq		loopMain
+	b		attempt
+
+
+
+haltLoop$:
+	b	haltLoop$
+
+
